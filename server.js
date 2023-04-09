295564c194c2059b4fe1d98bb5d82ebfbc032a49
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-04-08 17:58:02 -0700

    Added db functionality to image, id, and register endpoints

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express= require('express');
 const bcrypt= require('bcrypt'); 
 const cors= require('cors'); 
 const saltRounds = 1;
+const knex = require('knex');
+
+const databaseCon= knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'bensikora',
+      password : '',
+      database : 'smart-brain'
+    }
+  });
+
+databaseCon.select('*').from('users').then(data =>{
+    console.log(data);
+});
 
 
 
@@ -58,6 +74,17 @@ app.post('/signin', (req, res) =>{
 app.post('/register', (req, res) =>{
     const {name, email, password} = req.body; 
     bcrypt.hash(password, saltRounds, function(err, hash) {
+        databaseCon('users').returning('*').insert({
+            email: email, 
+            name: name, 
+            joined: new Date()
+
+        }).then(user =>{
+            res.json(user[0]);
+        }).catch(error =>{
+            res.status(400).json('unable to register');
+        });
+        /*
         database.users.push({
             id: '125',
             name: name,
@@ -65,27 +92,36 @@ app.post('/register', (req, res) =>{
             password: hash,
             entries: 0,
             joined: new Date(), 
-        })
-        res.json(database.users[database.users.length-1]); 
+        })*/
     }); 
 }); 
 
 app.get('/profile/:id', (req, res) => {
-    let found= false; 
-    database.users.forEach((user) => {
-        if(req.params.id == user.id){
-            found=true; 
-            return res.json(user); 
-        }
-    }); 
+    const {id}= req.params; 
 
-    if(!found){
-        res.status(404).json('user not found');
-    }
+    databaseCon.select('*').from('users').where({
+        id: id
+    }).then(user =>{
+        if(user.length != 0){
+            res.json(user[0]);
+        } else{
+            res.status(400).json('Not Found');
+        }
+    })
+
 
 }); 
 
 app.put('/image', (req, res) =>{
+    const{id} = req.body; 
+    databaseCon('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries').then(entries =>{
+        res.json(entries[0].entries);
+    }).catch(err =>{
+        res.status(400).json('unable to get count');
+    })
+    /*
     let found= false; 
     database.users.forEach((user) => {
         if(req.body.id == user.id){
@@ -97,7 +133,7 @@ app.put('/image', (req, res) =>{
 
     if(!found){
         res.status(404).json('user not found');
-    }
+    }*/
 }); 
 
 app.listen(3000, ()=>{
