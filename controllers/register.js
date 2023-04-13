7c745cb9593dadffbff3e3d0b480c841f2107e94
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-04-13 13:25:10 -0700

    Moved Register to own file

---
+++ controllers/register.js
@@ -0,0 +1,34 @@
+const handleRegister= (req, res, databaseCon, bcrypt) => {
+    const {name, email, password} = req.body;
+    const saltRounds= 10; 
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+    databaseCon.transaction(trx => {
+        trx.insert({
+            hash: hash, 
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users').returning('*').insert({
+                email: loginEmail[0].email, 
+                name: name, 
+                joined: new Date()
+    
+            }).then(user =>{
+                res.json(user[0]);
+            })
+
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(error =>{
+        res.status(400).json('unable to register');
+    });
+}
+
+module.exports= {
+    handleRegister: handleRegister
+};
