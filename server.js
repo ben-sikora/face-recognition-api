0522d3d013437cce9cf0713224fc374c4c88bffe
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-05-13 17:17:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ const image= require('./controllers/image.js');
 const databaseCon= knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      connectionString: process.env.DATABASE_URL,
+      host : process.env.DATABASE_HOST,
       port : 5432,
-      user : 'bensikora',
-      password : '',
-      database : 'smart-brain'
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PW,
+      database : process.env.DATABASE_DB
     }
   });
 
